db851a6168f3edf00de13d465469575e5c996a20
kevinclelland <kevinclelland@gmail.com>
2023-09-13 16:54:13 +0200

    Changed footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -88,21 +88,13 @@ function drawerOpen(): void {
 	
 <footer class="m-4">
     <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-      <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://saneleshange.com/" class="hover:underline">Sanele Shange</a>. Triathlete.
+      <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://saneleshange.com/" class="hover:underline">Sanele Shange</a>. Ironman Triathlete.
     </span>
     <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
         <li>
-            <a href="#" class="mr-4 hover:underline md:mr-6 ">About</a>
+            <a href="/" class="hover:underline">#BreakingLimits</a>
         </li>
-        <li>
-            <a href="#" class="mr-4 hover:underline md:mr-6">Privacy Policy</a>
-        </li>
-        <li>
-            <a href="#" class="mr-4 hover:underline md:mr-6">Licensing</a>
-        </li>
-        <li>
-            <a href="#" class="hover:underline">Contact</a>
-        </li>
+        
     </ul>
     </div>
 </footer>
