0b4439a5bfa7ef573d31c16313f1522eb7e10789
kevinclelland <kevinclelland@gmail.com>
2023-09-16 19:00:16 +0200

    Fixed padding

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,18 @@
 
 
 <div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-5">
+	<div class="space-y-4">
 		
 		<h1 class="h1 text-center p-4">#BreakingLimits</h1>
-		
-		<h2 class="h2 text-center max-w-2xl mx-auto">Hi 👋 I'm Sanele and I'm an <a href="https://en.wikipedia.org/wiki/Ironman_Triathlon">Ironman</a> Triathlete from Durban in South Africa.</h2>
-		<h2 class="h3 text-center max-w-4xl mx-auto">Through many years of dedication and hard work I have the unique achievement of being the first black African man to ever stand on an Ironman podium!</h2>
-		<h2 class="h3 text-center max-w-4xl mx-auto">I have qualified to race the World Champs twice, and with generous support, I was able to take up the opportunities and realize my dreams! Have a look at the results in the menu for details.</h2>
-		<h2 class="h3 text-center max-w-4xl mx-auto">I promote the "Breaking Limits" tagline as, personally, I have already broken through so many limits that have been placed on me...and I have big goals to break through many more. I aim to inspire everyone who follows my journey and supports me, but, I especially want to inspire my son Enzo.</h2>
-		<h2 class="h3 text-center max-w-4xl mx-auto">Please contact me on Instagram using the button below if you would like to get in touch:</h2>
-		<div class="flex mx-auto justify-center"><a href="https://www.instagram.com/sanele_ironman031/" class="btn variant-filled font-bold text-xl">Contact Me</a></div>
-		<img class="max-w-md mx-auto" src="/images/jan.png" alt="With Jan Frodeno">
-
+		<div class="flex grid grid-cols-1">
+		<h2 class="h2 text-center max-w-2xl mx-auto p-2">Hi 👋 I'm Sanele and I'm an <a href="https://en.wikipedia.org/wiki/Ironman_Triathlon">Ironman</a> Triathlete from Durban in South Africa.</h2>
+		<h2 class="h3 text-center max-w-4xl mx-auto p-2">Through many years of dedication and hard work I have the unique achievement of being the first black African man to ever stand on an Ironman podium!</h2>
+		<h2 class="h3 text-center max-w-4xl mx-auto p-2">I have qualified to race the World Champs twice, and with generous support, I was able to take up the opportunities and realize my dreams! Have a look at the results in the menu for details.</h2>
+		<h2 class="h3 text-center max-w-4xl mx-auto p-2">I promote the "Breaking Limits" tagline as, personally, I have already broken through so many limits that have been placed on me...and I have big goals to break through many more. I aim to inspire everyone who follows my journey and supports me, but, I especially want to inspire my son Enzo.</h2>
+		<h2 class="h3 text-center max-w-4xl mx-auto p-2">Please contact me on Instagram using the button below if you would like to get in touch:</h2>
+		<div class="flex mx-auto justify-center p-2"><a href="https://www.instagram.com/sanele_ironman031/" class="btn variant-filled font-bold text-xl">Contact Me</a></div>
+		<img class="max-w-md mx-auto p-2" src="/images/jan.png" alt="With Jan Frodeno">
+	</div>
 		
 		
 
