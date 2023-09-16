fe9802cf120eebbc4300cf66b2a6165eaab66a71
kevinclelland <kevinclelland@gmail.com>
2023-09-16 05:46:51 +0200

    Added results

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,11 @@
 		
 		<h1 class="h1 text-center p-4">#BreakingLimits</h1>
 		<h2 class="h2 text-center">Coming Soon</h2>
-		<img class="max-w-md" src="/images/jan.png" alt="Riding a bike in Nice">
+		<img class="max-w-md" src="/images/jan.png" alt="With Jan Frodeno">
+
+		
+		
+
 		
 	</div>
 
--- src/routes/results/+page.svelte
+++ src/routes/results/+page.svelte
@@ -1,1 +1,101 @@
 <h1 class="text-6xl p-4 text-center">Results</h1>
+
+<div class="max-w-2xl mx-auto text-center">
+    <p>I have been racing triathlon for many years now, but here are some highlights of my biggest races at the most competitive level.</p>
+</div>
+
+<div class="flex grid grid-cols-1 md:grid-cols-3 gap-4 p-4 mx-auto max-w-7xl">
+
+<div class="card card-hover max-w-sm">
+	<header class="card-header text-xl font-semibold">Ironman South Africa 2021</header>
+    <hr />
+	<section class="p-4">
+        <ul>
+            <li>Time: 08:41:12</li>
+            <li>Overall: 23</li>    
+            <li>Category: 2</li>
+            <li>Swim: 00:12:01</li>
+            <li>Bike: 05:11:03</li>
+            <li>Run: 03:11:24</li>
+        </ul>
+
+
+    </section>
+	<footer class="card-footer">Comments</footer>
+</div>
+	
+
+<div class="card card-hover max-w-sm">
+	<header class="card-header text-xl font-semibold">Ironman World Champs Kona 2022</header>
+    <hr />
+	<section class="p-4">
+        <ul>
+            <li>Time: 09:33:43</li>
+            <li>Overall: 281</li>    
+            <li>Category: 79</li>
+            <li>Swim: 01:01:54</li>
+            <li>Bike: 05:00:19</li>
+            <li>Run: 03:24:30</li>
+        </ul>
+
+
+    </section>
+	<footer class="card-footer">Comments</footer>
+</div>
+
+<div class="card card-hover max-w-sm">
+	<header class="card-header text-xl font-semibold">Ironman South Africa 2023</header>
+    <hr />
+	<section class="p-4">
+        <ul>
+            <li>Time: 08:53:16</li>
+            <li>Overall: 49</li>    
+            <li>Category: 4</li>
+            <li>Swim: 00:11:47</li>
+            <li>Bike: 05:09:49</li>
+            <li>Run: 03:26:03</li>
+        </ul>
+
+
+    </section>
+	<footer class="card-footer">Comments</footer>
+</div>
+
+
+<div class="card card-hover max-w-sm">
+	<header class="card-header text-xl font-semibold">Challenge Roth 2023</header>
+    <hr />
+	<section class="p-4">
+        <ul>
+            <li>Time: 09:15:41</li>
+            <li>Overall: 184</li>    
+            <li>Category: 48</li>
+            <li>Swim: 01:02:01</li>
+            <li>Bike: 04:56:54</li>
+            <li>Run: 03:11:25</li>
+        </ul>
+
+
+    </section>
+	<footer class="card-footer">Comments</footer>
+</div>
+
+<div class="card card-hover max-w-sm">
+	<header class="card-header text-xl font-semibold">Ironman World Champs Nice 2023</header>
+    <hr />
+	<section class="p-4">
+        <ul>
+            <li>Time: 10:03:53</li>
+            <li>Overall: 125</li>    
+            <li>Category: 26</li>
+            <li>Swim: 01:03:27</li>
+            <li>Bike: 05:36:23</li>
+            <li>Run: 03:15:30</li>
+        </ul>
+
+
+    </section>
+	<footer class="card-footer">Comments</footer>
+</div>
+
+</div>
