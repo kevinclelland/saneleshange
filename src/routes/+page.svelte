daeb78b370d7475e3869a2ad004fc66a19f98e52
kevinclelland <kevinclelland@gmail.com>
2023-09-17 10:09:19 +0200

    Changed ciovita logo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,10 +7,10 @@
 			<h1 class="h1 text-center p-4">#BreakingLimits</h1>
 
 			<div class="flex grid grid-cols-1">
-				<h2 class="h2 text-center max-w-2xl mx-auto p-2">Hi 👋 I'm Sanele and I'm an <a href="https://en.wikipedia.org/wiki/Ironman_Triathlon">Ironman</a> Triathlete from Durban in South Africa.</h2>
+				<h2 class="h2 text-center max-w-2xl mx-auto p-2">Hi 👋 I'm Sanele and I'm an <a href="https://en.wikipedia.org/wiki/Ironman_Triathlon">Ironman</a> Triathlete from Durban, South Africa.</h2>
 				<h2 class="h3 text-center max-w-4xl mx-auto p-2">Through many years of dedication and hard work I have the unique achievement of being the first black African man to ever stand on an Ironman podium!</h2>
-				<h2 class="h3 text-center max-w-4xl mx-auto p-2">I have qualified to race the World Champs twice, and with generous support, I was able to take up the opportunities and realize my dreams! Have a look at the results in the menu for details.</h2>
-				<h2 class="h3 text-center max-w-4xl mx-auto p-2">I promote the "Breaking Limits" tagline as, personally, I have already broken through so many limits that have been placed on me...and I have big goals to break through many more. I aim to inspire everyone who follows my journey and supports me, but, I especially want to inspire my son Enzo.</h2>
+				<h2 class="h3 text-center max-w-4xl mx-auto p-2">I have qualified and raced the World Championships twice, enabled by the generous support of my sponsors.</h2>
+				<h2 class="h3 text-center max-w-4xl mx-auto p-2">I promote the "Breaking Limits" tagline as I have broken through so many limits that have been placed on me...and I aim to break through many more. I want to inspire everyone who follows my journey, but, I especially want to inspire my son Enzo.</h2>
 				<h2 class="h3 text-center max-w-4xl mx-auto p-2">Follow or contact me on Instagram below:</h2>
 				<div class="flex mx-auto justify-center p-2"><a href="https://www.instagram.com/sanele_ironman031/" class="btn variant-filled font-bold text-xl">
 				
@@ -19,14 +19,14 @@
 
 				</a></div>
 			
-				<img class="max-w-sm mx-auto p-2" src="/images/gallery/jan_2.webp" alt="With Jan Frodeno">
+				<img class="max-w-sm mx-auto p-2 rounded-3xl" src="/images/gallery/jan_2.webp" alt="With Jan Frodeno">
 			</div>
 		
 			<div class="flex grid grid-cols-2 md:grid-cols-5 items-center justify-center gap-4">
 				<a href="https://hookeisa.co.za/" target="_blank" class="hidden dark:block"><img src="/images/hookeilogo.png" alt="Hookei Logo"></a>
 				<a href="https://hookeisa.co.za/" target="_blank" class="dark:hidden block"><img src="/images/hookeilogodark.png" alt="Hookei Logo"></a>
-				<a href="https://ciovita.co.za/" target="_blank" class="hidden dark:block"><img src="/images/ciovitalogodark.png" alt="Ciovita Logo"></a>
-				<a href="https://ciovita.co.za/" target="_blank" class="dark:hidden block"><img src="/images/ciovitalogo.png" alt="Ciovita Logo"></a>
+				<a href="https://ciovita.co.za/" target="_blank" class="hidden dark:block"><img src="/images/ciovitalogo.webp" alt="Ciovita Logo"></a>
+				<a href="https://ciovita.co.za/" target="_blank" class="dark:hidden block"><img src="/images/ciovitalogodark.webp" alt="Ciovita Logo"></a>
 				<a href="https://32gi.com/" target="_blank" class="hidden dark:block"><img src="/images/32gilogodark.webp" alt="32GI Logo"></a>
 				<a href="https://32gi.com/" target="_blank" class="dark:hidden block"><img src="/images/32gilogo.webp" alt="32GI Logo"></a>
 				<a href="https://light-speed.co.za/" target="_blank" class="hidden dark:block"><img src="/images/lightspeedlogo.png" alt="Lightspeed Logo"></a>
